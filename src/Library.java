0ee5450b955d728790f4a7573e7a2fd51a1cdee9
James Kelly <jamesekelly3@gmail.com>
2016-04-20 15:34:17 -0400

    Deleted BookIO, Created getLibrary)

--- src/Library.java
+++ src/Library.java
@@ -1,4 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
-public class Library {
+public class Library 
+{
+static ArrayList<Book> myLibrary= new ArrayList<>();
+public static void getLibrary() {
+	
+	Path librariesPath = Paths.get("library.txt");
+	File librariesFile = librariesPath.toFile();
+	try {
+	FileReader r = new FileReader(librariesFile);
+	BufferedReader in = new BufferedReader( r);
+	String line = in.readLine();
+	while (line !=null)
+	{
+		String delims = "[,]";
+		String[] list = line.split(delims);
+		myLibrary.add(new Book(list[0],list[1],list[2],list[3],list[4]));
+	
+	line = in.readLine();
+	}
+	in.close();
+	}
+	catch (IOException e)
+	{
+	System.out.println(e);
+	}
 
+}
 }
